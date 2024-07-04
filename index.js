77b70621f0c5b90b012acd9ad3125b804fdbbb33
Suraz95 <shaik.suraz50@gmail.com>
2024-07-04 11:50:08 +0530

    hbch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const app=express()
 const port=8000;
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.send("Hello World")
 })
 app.listen(port,()=>{
     console.log(`server is running at ${port}`);
